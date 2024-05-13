fe5163f2284c72efd35d2cdc57ecf95fbed7b45b
timothySyu <a0960622153@gmail.com>
2024-05-13 18:16:57 +0800

    1. 新增神話騎士變身 2. 新增真冥煌弓 3.修正妖精無法三重問題 4.整理db

---
+++ dev_log/DB_新增/beginner_修改.sql
@@ -0,0 +1,6 @@
+insert into beginner (item_id, count, charge_count, enchantlvl, item_name, activate, bless) values ('40024', '500', '0', '0', '古代終極體力恢復劑', 'A', '0');
+insert into beginner (item_id, count, charge_count, enchantlvl, item_name, activate, bless) values ('40079', '100', '0', '0', '傳送回家的卷軸', 'A', '0');
+insert into beginner (item_id, count, charge_count, enchantlvl, item_name, activate, bless) values ('40117', '100', '0', '0', '銀騎士村莊指定傳送卷軸', 'A', '0');
+insert into beginner (item_id, count, charge_count, enchantlvl, item_name, activate, bless) values ('40126', '100', '0', '0', '鑑定卷軸', 'A', '0');
+insert into beginner (item_id, count, charge_count, enchantlvl, item_name, activate, bless) values ('40733', '100', '0', '0', '名譽貨幣', 'A', '0');
+insert into beginner (item_id, count, charge_count, enchantlvl, item_name, activate, bless) values ('40100', '500', '0', '0', '瞬間移動卷軸', 'A', '1');
---
+++ dev_log/DB_新增/drop_list_新增.sql
@@ -0,0 +1,10 @@
+-- 奇岩地監怪物掉落金幣新增
+insert into droplist values ('97052', '40308', '100', '200', '1000000');
+insert into droplist values ('97053', '40308', '100', '200', '1000000');
+insert into droplist values ('97054', '40308', '100', '200', '1000000');
+insert into droplist values ('97058', '40308', '100', '200', '1000000');
+insert into droplist values ('97059', '40308', '100', '200', '1000000');
+insert into droplist values ('97060', '40308', '100', '200', '1000000');
+insert into droplist values ('97063', '40308', '100', '200', '1000000');
+insert into droplist values ('97064', '40308', '100', '200', '1000000');
+insert into droplist values ('97066', '40308', '100', '200', '1000000');
